1128fa4d31499194c6f35e72b91d47ddcb55e9bb
hugosenari <hugosenari@gmail.com>
2022-05-09 20:36:04 -0300

    chore: re index cli_menu

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."cli_menu-master".dir   = "master";
-  inputs."cli_menu-master".owner = "nim-nix-pkgs";
-  inputs."cli_menu-master".ref   = "master";
-  inputs."cli_menu-master".repo  = "cli_menu";
-  inputs."cli_menu-master".type  = "github";
-  inputs."cli_menu-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."cli_menu-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
